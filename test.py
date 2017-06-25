93e3e3fbe1af506b8f1578d9300c349722fd4bae
Tim Smith <timsmith714@gmail.com>
2017-06-25 21:51:00 +0000

    Tried to do comparison

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from time import sleep
 from UUGear import *
+import sys
 
 print 'starting'
 
@@ -11,14 +12,22 @@ try:
     if device.isValid():
         for i in range(10):
             print str(i)
-            print "Device 1: %0.2f" % (float(device.analogRead(0)) * 3.3 / 1024), "V"
-            print "Device 2: %0.2f" % (float(device.analogRead(1)) * 3.3 / 1024), "V"
+            analog_one = float(device.analogRead(3)) * 3.3 / 1024
+            analog_two = float(device.anaogRead(4)) * 3.3 / 1024
+            if analog_one > analog_two:
+                print "On"
+            else:
+                print "Off"
+
+            print "Device 1: %0.2f" % (float(device.analogRead(3)) * 3.3 / 1024), "V"
+            print "Device 2: %0.2f" % (float(device.analogRead(4)) * 3.3 / 1024), "V"
 	    sleep(0.5)
 
 	device.detach()
 	device.stopDaemon()
     else:
 	print 'UUGear device is not currently installed'
-except(e):
-    print 'Error:' + str(e)
+except:
+    #print 'Error:' + str(e)
+    print "Unexpected error:", sys.exc_info()[0]
 
