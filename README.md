9652fc206b6c13b1c9bb414c4ee7ea1f073bc8a4
Tim Smith <timsmith714@gmail.com>
2017-07-04 21:28:55 +0000

    Added music code and a quit function

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Pi-Arduino-MusicJokeBox
-Use a Pi and an Arduino to play music when the box comes into the light 
+Use a Pi and an Arduino to play music when the box comes into the light
+
+1. Assumes that you've cloned the UUGear utility from https://github.com/uugear/UUGear into the same folder that hosts this project. For example I have a folder called GitProjects that has both cloned repositories
--- test.py
+++ test.py
@@ -1,29 +1,52 @@
 from time import sleep
 from UUGear import *
 import sys
+import pygame
+import datetime
+import os
 
-print 'starting'
+def quit():
+    pygame.mixer.music.stop()
+    device.detach()
+    device.stopDaemon()
+    pygame.quit()
+    print("Script Finished")
+    exit()
 
+print 'starting init'
+pygame.init()
 UUGearDevice.setShowLogs(0)
+fname = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M') + "-log.txt"
+open(fname, 'a')
+
 
 try: 
     device = UUGearDevice('UUGear-Arduino-8483-2314')
 
     if device.isValid():
-        for i in range(10):
+        #Init music
+        pygame.mixer.music.load('track01.mp3')
+        pygame.mixer.music.play()
+        pygame.mixer.music.pause()
+        for i in range(30):
             print str(i)
             if float(device.analogRead(3)) > float(device.analogRead(4)):
                 print "On"
+                if pygame.mixer.music.get_busy == False:
+                    pygame.mixer.music.unpause()
             else:
                 print "Off"
+                if pygame.mixer.music.get_busy == True:
+                    pygame.mixer.music.pause()
 
             print "Device 1: %0.2f" % (float(device.analogRead(3)) * 3.3 / 1024), "V"
             print "Device 2: %0.2f" % (float(device.analogRead(4)) * 3.3 / 1024), "V"
+            f = open(fname, 'ab')
+            f.write(datetime.datetime.now().strftime('%Y-%m-%dT%H:%M.%S') + "\n")
+            f.close()
 	    sleep(0.5)
 
-	device.detach()
-	device.stopDaemon()
-        print 'script finished'
+	quit()
     else:
 	print 'UUGear device is not currently installed'
 except:
